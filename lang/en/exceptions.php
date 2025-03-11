529d9f1131af0431d9b1fd6ab4ddfef9dd70189c
acer <rahmanimahdi16@gmail.com>
2025-03-11 15:07:34 +0330

    add pagination

---
+++ lang/en/exceptions.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'wallet_already_exists_for_user' => 'user with id :user_id already have a wallet available',
+    'withdraw_amount_is_bigger_than_wallet_balance' => 'withdraw amount is bigger than wallet balance',
+];
